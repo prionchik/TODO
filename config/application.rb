cc1370376a9dddb4edb5781defd5d55f30719791
Alexandr Fedotov <fedotov0763@gmail.com>
2019-09-25 18:25:46 +0300

    q

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
 require_relative 'boot'
 
 require 'rails/all'
-
+require 'active_support'
+require 'active_support/core_ext'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+data = HashWithIndifferentAccess.new(YAML::load_file(File.join(Rails.root, 'db', 'seeds.yml')))
 
-#seed_file = Rails.root.join('db', 'seeds.yml')
-#config = YAML::load_file(seed_file)
-#print(config)
-#Project.create!(config)
-# db/seeds.rb
-require 'yaml'
-# Парсим данные из yml файла
-data = YAML.load_file('db/seeds.yml')
-# Получаем массив проектов (получается массив хешей)
-#print(data['projects'])
-projects = data['projects']
-#print(projects)
-#print('hi')
-# Для каждого проекта из массива:
-projects.each do |project|
-  # Создаем новый проект в БД
-  puts project['title']
-  created_project = Project.create(title: project['title'])
-  # Создаем для нового проекта todos
-  #created_project.todos.create(project['todos'])
-  puts created_project
-  project['todos'].each do |w|
-    created_project.dos.create(text: w['text'],isCompleted: w['isCompleted'])
-  end
+data[:projects].each do |project|
+        p = Project.new(title: project[:title])
+        puts project[:todos]
+        project[:todos].each do |todo|
+          p.dos << Do.create(todo)
+        end
+        p.save!
 end
